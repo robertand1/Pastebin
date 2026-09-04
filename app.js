2b24312ee7125d3ec3905030a285c0eda3f7c5fa
robertand1 <126794012+robertand1@users.noreply.github.com>
2026-09-04 15:23:03 +0300

    Add express-layouts middleware for layout support

--- app.js
+++ app.js
@@ -5,5 +5,7 @@ const app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
+app.use(expressLayouts);
+app.set("layout", "layout");
 app.use("/", indexRouter);
 module.exports = app;
