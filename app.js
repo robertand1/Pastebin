0356b028a92ab43bea561a0ecf9f97b58ca99b03
robertand1 <126794012+robertand1@users.noreply.github.com>
2026-09-04 16:05:55 +0300

    Add express-ejs-layouts for layout support

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const path = require("path");
+const expressLayouts = require("express-ejs-layouts");
 const indexRouter = require("./routes/index");
 const app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
-app.use(express.urlencoded({ extended: true }));
 app.use(expressLayouts);
 app.set("layout", "layout");
+app.use(express.urlencoded({ extended: true }));
 app.use("/", indexRouter);
 module.exports = app;
