f0b48203c2f88be6f0fcc42fac6a0087285aede5
robertand1 <126794012+robertand1@users.noreply.github.com>
2026-09-05 15:20:19 +0300

    Refactor database connection and add TextModel methods

--- helpers/database.js
+++ helpers/database.js
@@ -1,11 +1,8 @@
 const sqlite3 = require("sqlite3").verbose();
 
 const db = new sqlite3.Database("./database.db", (err) => {
-  if (err) {
-    console.error("Error connecting to the database:", err.message);
-  } else {
-    console.log("Connected to the SQLite database.");
-  }
+  if (err) console.error("Error connecting to the database:", err.message);
+  else console.log("Connected to the SQLite database.");
 });
 
 db.serialize(() => {
@@ -14,4 +11,18 @@ db.serialize(() => {
   );
 });
 
-module.exports = db;
+const TextModel = {
+  getAll: (callback) => {
+    db.all("SELECT * FROM saved_texts ORDER BY id DESC", [], callback);
+  },
+  add: (content, callback) => {
+    const insertQuery = "INSERT INTO saved_texts (content) VALUES (?)";
+    db.run(insertQuery, [content], callback);
+  },
+  getById: (id, callback) => {
+    const selectQuery = "SELECT * FROM saved_texts WHERE id = ?";
+    db.get(selectQuery, [id], callback);
+  },
+};
+
+module.exports = TextModel;
