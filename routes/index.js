96617a5c8c33e423a25ba175a334a494bfdfe05c
robertand1 <126794012+robertand1@users.noreply.github.com>
2026-09-05 15:19:44 +0300

    Refactor database operations to use TextModel

--- routes/index.js
+++ routes/index.js
@@ -1,27 +1,25 @@
 const express = require("express");
 const router = express.Router();
-const db = require("../helpers/database");
+const TextModel = require("../helpers/database");
 
 router.get("/", (req, res) => {
-  db.all("SELECT * FROM saved_texts ORDER BY id DESC", [], (err, rows) => {
+  TextModel.getAll((err, rows) => {
     res.render("index", { texts: rows });
   });
 });
 
 router.post("/add", (req, res) => {
-  const insertQuery = "INSERT INTO saved_texts (content) VALUES (?)";
-  db.run(insertQuery, [req.body.content], function (err) {
+  TextModel.add(req.body.content, (err) => {
     res.redirect("/");
   });
 });
 
 router.get("/text/:id", (req, res) => {
-  const selectQuery = "SELECT * FROM saved_texts WHERE id = ?";
-  db.get(selectQuery, [req.params.id], (err, row) => {
+  TextModel.getById(req.params.id, (err, row) => {
     if (row) {
       res.render("text", { text: row });
     } else {
-      res.send("404");
+      res.send("");
     }
   });
 });
